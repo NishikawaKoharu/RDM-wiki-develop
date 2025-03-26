79000d1bfd26df9367e3da4a4c66792a65bb55eb
NishikawaKoharu <nishikawa@omik.co.jp>
2025-03-26 10:23:04 +0900

    #51456

--- addons/wiki/static/wikiPageMilkdown.js
+++ addons/wiki/static/wikiPageMilkdown.js
@@ -1234,7 +1234,13 @@ window.onload = () => {
         // 現在のURLにハッシュが含まれているか確認
         if (window.location.hash) {
             await pFiveSleep();
-            const target = document.querySelector(window.location.hash);
+            var target = ""
+            try{
+                const decodedURI = decodeURI(window.location.hash);
+                target = document.querySelector(decodedURI);
+            } catch (error) {
+                target = document.querySelector(window.location.hash);
+            }
             if (target) {
                 // 目的の要素へスクロール
                 target.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -1245,4 +1251,3 @@ window.onload = () => {
 }
 //#46532 対応 Add End
 export default WikiPageMilkdown;
-
